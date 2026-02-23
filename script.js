17e6e949ee7e05b7e8ef25b74c51c53e483eb851
badralboqmi-max <263206385+badralboqmi-max@users.noreply.github.com>
2026-02-23 05:05:56 +0300

    From zero

---
+++ script.js
@@ -0,0 +1,9 @@
+// تمرير سلس عند النقر على الروابط
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
